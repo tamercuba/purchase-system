bb911ae2606433e1109e7560f89529523d6425a0
tamer.cuba <tamercuba@gmail.com>
2021-04-15 17:47:26 -0300

    STYLE: CreateNewSalesman renamed to CreateSalesman

--- purchase_system/domain/services/__init__.py
+++ purchase_system/domain/services/__init__.py
@@ -1,8 +1,8 @@
-from domain.services.create_new_salesman import (
-    CreateNewSalesman,
-    NewSalesmanRequest,
-    NewSalesmanResponse,
-)
 from domain.services.create_sale import CreateSaleRequest, CreateSaleService
+from domain.services.create_salesman import (
+    CreateSalesman,
+    CreateSalesmanRequest,
+    CreateSalesmanResponse,
+)
 from domain.services.delete_sale import DeleteSaleRequest, DeleteSaleService
 from domain.services.update_sale import UpdateSale, UpdateSaleRequest
