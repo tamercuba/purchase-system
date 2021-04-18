74818aedb104024137c796d9e0e70e3da3deabe0
tamer.cuba <tamercuba@gmail.com>
2021-04-18 15:13:28 -0300

    FEATUER: cashback route added

--- api/adapters/services.py
+++ api/adapters/services.py
@@ -3,6 +3,7 @@ from purchase_system.domain.services import (
     CreateSaleService,
     CreateSalesman,
     DeleteSaleService,
+    GetSalesmanCashback,
     ListSales,
     UpdateSale,
 )
@@ -12,3 +13,4 @@ create_salesman_service = CreateSalesman(salesman_repository)
 delete_sale_service = DeleteSaleService(sale_repository, salesman_repository)
 list_sales_service = ListSales(sale_repository, salesman_repository)
 update_sale_service = UpdateSale(sale_repository, salesman_repository)
+get_user_cashback_service = GetSalesmanCashback(sale_repository)
